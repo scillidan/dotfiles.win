311059f363e530435d740cc886d21b877ebd3e0c
scillidan <scillidan@yandex.com>
2026-06-07 20:47:02 +0800

    .

--- Scoop/clink/user_aliases.cmd
+++ Scoop/clink/user_aliases.cmd
@@ -2,6 +2,7 @@
 
 for %%f in (
 	"%USERHOME%\Share\dotfiles\.config\alias.yaml"
+	"%USERHOME%\Share\dotfiles\.config\alias-scripts.yaml"
 	"%USERHOME%\Share\dotfiles.win\.config\alias.yaml"
 ) do if exist "%%f" (
 	for /f "tokens=1,2 delims=@" %%a in ('yq -o props "... comments = """"" --properties-separator="@" "%%f"') do (
